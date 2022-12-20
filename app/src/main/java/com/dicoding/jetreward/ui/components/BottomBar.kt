ec490dd2bb6722752fc9457a5f60851bf78ee629
ilhamelmujib <ilham.elmujib@gmail.com>
2022-12-20 15:20:04 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/jetreward/ui/components/BottomBar.kt
@@ -0,0 +1,86 @@
+package com.dicoding.jetreward.ui.components
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.dicoding.jetreward.R
+import com.dicoding.jetreward.ui.navigation.NavigationItem
+import com.dicoding.jetreward.ui.navigation.Screen
+import com.dicoding.jetreward.ui.theme.JetRewardTheme
+
+@Composable
+fun BottomBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_cart),
+                icon = Icons.Default.ShoppingCart,
+                screen = Screen.Cart
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            ),
+        )
+        BottomNavigation {
+
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+
+            navigationItems.map { item ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title
+                        )
+                    },
+                    label = { Text(item.title) },
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomBarPreview() {
+    JetRewardTheme {
+        BottomBar(rememberNavController())
+    }
+}
